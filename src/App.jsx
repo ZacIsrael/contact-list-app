16625a94cf027415d11c2f5b53e3296448337dac
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-09 19:42:16 -0400

    Successfully implemented the Card componet; used props object to make it dynamic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // In a real application, this data would get fetched from the server
+  const contacts = [
+    {
+      name: "Beyonce",
+      imgURL:
+        "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+      phone: "+123 456 789",
+      email: "b@beyonce.com",
+    },
+    {
+      name: "Jack Bauer",
+      imgURL:
+        "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+      phone: "+987 654 321",
+      email: "jack@nowhere.com",
+    },
+    {
+      name: "Chuck Norris",
+      imgURL:
+        "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+      phone: "+918 372 574",
+      email: "gmail@chucknorris.com",
+    },
+  ];
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="heading">My Contacts</h1>
+        {/* Creat a Card element for each contact */}
+        {contacts.map((item, i) => (
+          <Card
+            key={i}
+            contactName={item.name}
+            imageURL={item.imgURL}
+            phoneNumber={item.phone}
+            email={item.email}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Card (props){
+    console.log('Card component: props = ', props);
+    return (
+        <div className="card">
+            <div className="top">
+                <h2>{props.contactName}</h2>
+                <img src={props.imageURL} alt={"image of " + props.contactName}/>
+            </div>
+            <div className="bottom">
+                <p>{props.phoneNumber}</p>
+                <p>{props.email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
