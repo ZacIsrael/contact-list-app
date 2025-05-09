783242d92d238bc62942c52f48a31963c72b57c0
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-09 19:47:06 -0400

    added classes to the name and image in the Card component for styling purposes.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,8 @@ function Card (props){
     return (
         <div className="card">
             <div className="top">
-                <h2>{props.contactName}</h2>
-                <img src={props.imageURL} alt={"image of " + props.contactName}/>
+                <h2 className="name">{props.contactName}</h2>
+                <img className="circle-img" src={props.imageURL} alt={"image of " + props.contactName}/>
             </div>
             <div className="bottom">
                 <p>{props.phoneNumber}</p>
