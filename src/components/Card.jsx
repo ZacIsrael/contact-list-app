a420bbbc686fa1ff441f82e494b8b5e3654642f4
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-05-09 20:10:38 -0400

    Added styling to the bottom elements in the Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Avatar from "./Avatar";
 
-function Card (props){
-    console.log('Card component: props = ', props);
-    return (
-        <div className="card">
-            <div className="top">
-                <h2 className="name">{props.contactName}</h2>
-                <Avatar imageURL={props.imageURL} />
-                {/* <img className="circle-img" src={props.imageURL} alt={"image of " + props.contactName}/> */}
-            </div>
-            <div className="bottom">
-                <p>{props.phoneNumber}</p>
-                <p>{props.email}</p>
-            </div>
-        </div>
-    );
+function Card(props) {
+  console.log("Card component: props = ", props);
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.contactName}</h2>
+        <Avatar imageURL={props.imageURL} />
+        {/* <img className="circle-img" src={props.imageURL} alt={"image of " + props.contactName}/> */}
+      </div>
+      <div className="bottom">
+        <p className="info">{props.phoneNumber}</p>
+        <p className="info">{props.email}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
